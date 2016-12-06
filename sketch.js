2ec3168c8f8c0bd04daa3e87c78a2511ff05a88d
sarapizzatti93 <sara.pizzatti@gmail.com>
2016-12-06 16:12:11 +0100

    r

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function draw() {
         var touch=touches[i];
         var touchColor=colorList[i]
         fill(touchColor);
-        ellipse(touch.x, touch.y, 150, 140);
+        ellipse(touch.x, touch.y, 50, 50);
 
     }
 }
